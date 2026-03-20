d9557ee6f4263363f1415e0fad08a5a13157b251
fuad <fuadshorlarxwix@gmail.com>
2026-03-20 19:27:54 +0100

    frame fixed

--- src/Header.jsx
+++ src/Header.jsx
@@ -21,32 +21,32 @@ export default function header() {
       <nav className={isOpen ? "nav-menu open" : "nav-menu hidden"}>
         <ul>
           <li>
-            <a href="#my-home" onClick={() => setIsOpen(true)}>
+            <a href="#my-home" onClick={() => setIsOpen(false)}>
               Home
             </a>
           </li>
           <li>
-            <a href="#about-me" onClick={() => setIsOpen(true)}>
+            <a href="#about-me" onClick={() => setIsOpen(false)}>
               About
             </a>
           </li>
           <li>
-            <a href="#my-service" onClick={() => setIsOpen(true)}>
+            <a href="#my-service" onClick={() => setIsOpen(false)}>
               Services
             </a>
           </li>
           <li>
-            <a href="#my-skills" onClick={() => setIsOpen(true)}>
+            <a href="#my-skills" onClick={() => setIsOpen(false)}>
               Skills
             </a>
           </li>
           <li>
-            <a href="#my-jobs" onClick={() => setIsOpen(true)}>
+            <a href="#my-jobs" onClick={() => setIsOpen(false)}>
               Jobs
             </a>
           </li>
           <li>
-            <a href="#contact-me" onClick={() => setIsOpen(true)}>
+            <a href="#contact-me" onClick={() => setIsOpen(false)}>
               Contact
             </a>
           </li>
